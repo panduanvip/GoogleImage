1c221ba9a11871ad67b52ff9d1a4b176c61f6fc5
Panduan.VIP <webpanduanvip@gmail.com>
2021-06-14 01:54:47 +0700

    Add Curl Method

--- src/GoogleImage.php
+++ src/GoogleImage.php
@@ -7,8 +7,10 @@ use \RoNoLo\JsonExtractor\JsonExtractorService;
 class GoogleImage
 {
 
-    public static function extractor($html)
+    public static function get($keyword, $proxy='')
     {
+		$html = self::curl($keyword, $proxy);
+
 		$jsonExtractor = new JsonExtractorService();
 		$data = $jsonExtractor->extractAllJsonData($html);
 
@@ -47,5 +49,37 @@ class GoogleImage
 		
         return json_encode($results);
     }
+	
+	private static function curl($keyword, $proxy='')
+	{
+		if (!function_exists('curl_version')) {
+			die('cURL extension is disabled on your server!');
+		}
+
+		$keyword = str_replace(' ', '+', $keyword);
+		$url = "https://www.google.com/search?q=$keyword&source=lnms&tbm=isch&safe=strict&tbs=isz:l";
+
+		$user_agent = 'Mozilla/5.0 (Linux; Android 10; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36.';
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);	
+		curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+		if (isset($_SERVER['HTTP_REFERER'])) {
+			curl_setopt($ch, CURLOPT_REFERER, $_SERVER['HTTP_REFERER']);
+		}
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+		if (!empty($proxy)) {
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+		}
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return $result;
+	}
   
 }
--- test.php
+++ test.php
@@ -11,14 +11,8 @@ if(! class_exists('RoNoLo\JsonExtractor\JsonExtractorService')){
 	include_once '../ronolo-json-extract/src/JsonExtractorException.php';
 }
 
-$url = 'https://www.google.com/search?q=sepatu+roda&source=lnms&tbm=isch';
-$user_agent = 'Mozilla/5.0 (Linux; Android 10; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36.';
-
-$options  = array('http' => array('user_agent' => $user_agent));
-$context  = stream_context_create($options);
-$html = file_get_contents($url, false, $context);
-
-$results = json_decode(GoogleImage::extractor($html));
+$keyword = 'sepatu roda';
+$results = json_decode(GoogleImage::get($keyword));
 
 echo '<pre>';
 print_r($results);
